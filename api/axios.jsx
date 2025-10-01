00d817ca63de03cc81caf9b554a363b3b8bcdcb7
Kevin <kevin.alexanderra1302@gmail.com>
2025-10-01 21:41:53 +0100

    v2produccion

--- api/axios.jsx
+++ api/axios.jsx
@@ -1,7 +1,18 @@
 import axios from 'axios';
 
+const currentHostname = window.location.hostname;
+
+const productionBaseURL = 'https://server-mern-sell-all.vercel.app/';
+
+const localBaseURL = 'http://localhost:3000/';
+
+const baseURL =
+  currentHostname === 'localhost' || currentHostname === '127.0.0.1'
+    ? localBaseURL
+    : productionBaseURL;
+
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: baseURL,
   withCredentials: true,
 });
 
